ed0eca4ee041b0fe133bf0db9313ead7c0bedf47
AnnBalias <annbalias@gmail.com>
2025-06-29 21:22:29 +0300

    clsx-fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,18 @@ export default function Home() {
   return (
     <main>
       <h1 className="text-xl">Home page</h1>
-      <StatusLabel status={Status.Active}>Active</StatusLabel>
-      <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
-      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+      <StatusLabel status={Status.Active} disabled={false}>
+        Active
+      </StatusLabel>
+      <StatusLabel status={Status.NotActive} disabled={true}>
+        Not Active
+      </StatusLabel>
+      <StatusLabel status={Status.Pending} disabled={false}>
+        Pending
+      </StatusLabel>
+      <StatusLabel status={Status.Suspended} disabled={true}>
+        Suspended
+      </StatusLabel>
     </main>
   );
 }
